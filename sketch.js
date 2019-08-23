f05abb60ab406ba3ae99ce46f09c1ce9f292c17e
shivanshu1707 <54066750+shivanshu1707@users.noreply.github.com>
2019-08-23 19:05:51 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ function draw() {
   if(movingRect.x-fixedRect.x < fixedRect.width/2+movingRect.width/2
     && fixedRect.x-movingRect.x < fixedRect.width/2+movingRect.width/2
     && movingRect.y-fixedRect.y < fixedRect.width/2+movingRect.width/2
-    && fixedRect.x-movingRect.x < fixedRect.width/2+movingRect.width/2) {
+    && fixedRect.y-movingRect.y < fixedRect.width/2+movingRect.width/2) {
     movingRect.shapeColor = "red";
     fixedRect.shapeColor = "red";
   } else {
