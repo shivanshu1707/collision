49921b0f42d69f37412504946d8081978fca3900
shivanshu1707 <54066750+shivanshu1707@users.noreply.github.com>
2019-09-01 11:00:31 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,34 +3,40 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var ground,ball;g
+var ground,ball,box1,box2;
 
 function setup(){
     var canvas = createCanvas(400,400);
     engine = Engine.create();
     world = engine.world;
+    box1 = new Box(200,300,50,100);
+    box2 = new Box(240,100,50,100);
+    ground = new Ground(200,390,400,20);
 
-    var ground_options ={
-        isStatic: true
-    }
+  //  var ground_options ={
+  //      isStatic: true
+  //  }
 
-    ground = Bodies.rectangle(200,390,200,20,ground_options);
-    World.add(world,ground);
+  //  ground = Bodies.rectangle(200,390,200,20,ground_options);
+  //  World.add(world,ground);
 
-    var ball_options = {
-        restitution:1.0
-    }
+  //  var ball_options = {
+  //      restitution:1.0
+   // }
 
-    ball = Bodies.circle(200,100,20,ball_options);
-    World.add(world,ball);
-    console.log(ground);
+  //  ball = Bodies.circle(200,100,20,ball_options);
+  //  World.add(world,ball);
+  //  console.log(ground);
 }
 
 function draw(){
     background(0);
     Engine.update(engine);
-    rectMode(CENTER);
-    rect(ground.position.x,ground.position.y,400,20);
-    ellipseMode(RADIUS);
-    ellipse(ball.position.x,ball.position.y,20,20);
+    box1.display();
+    box2.display();
+    ground.display();
+  //  rectMode(CENTER);
+  //  rect(ground.position.x,ground.position.y,400,20);
+  //  ellipseMode(RADIUS);
+  //  ellipse(ball.position.x,ball.position.y,20,20);
 }
